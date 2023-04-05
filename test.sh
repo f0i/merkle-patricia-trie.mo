7557cc9caeb2b44c8b4e26fdc01a0a0681bce26f
Martin Sigloch <f0i.git@projects.f0i.de>
2023-04-05 20:18:02 +0200

    Update test error message

--- test.sh
+++ test.sh
@@ -4,5 +4,6 @@ set -eu -o pipefail
 
 ($(vessel bin)/moc $(vessel sources) -r "test/index.spec.mo" $* | tee test/RESULTS.md) \
     || (echo -e '\n\n# Test Result \n\n' | tee -a test/RESULTS.md ; false) \
-    || (echo -e '    ❌ Test failed!' | tee -a test/RESULTS.md ; false) \
-    || (echo -e '' | tee -a test/RESULTS.md ; false) \
+    || (echo -e '    ┌─────────────────╖' | tee -a test/RESULTS.md ; false) \
+    || (echo -e '    │ ❌ Test failed! ║' | tee -a test/RESULTS.md ; false) \
+    || (echo -e '    ╘═════════════════╝\n' | tee -a test/RESULTS.md ; false) \
